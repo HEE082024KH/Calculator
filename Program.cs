3c0a226ab55dd3acd8ee24b1d788400262e3e845
hee082024kh <hee082024@gmail.com>
2024-09-17 13:14:42 +0200

    Initialized class in main scope

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 {
     static void Main(string[] args)
     {
+        Calculator calculator = new Calculator();
+        Console.WriteLine(calculator.Add(1, 1));
+        Console.WriteLine(calculator.Subtract(70, 1));
 
     }
 }

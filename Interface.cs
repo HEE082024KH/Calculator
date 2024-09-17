514e1a957a015814e1d2b4c439d3b3e611df2dd3
hee082024kh <hee082024@gmail.com>
2024-09-17 13:13:36 +0200

    Initial Commi

---
+++ Interface.cs
@@ -0,0 +1,14 @@
+public interface ICalculator
+{
+  /// <summary>Add the doubles a and b</summary>
+  /// <param name="a">the double a</param>
+  /// <param name="b">the double b</param>
+  /// <returns>a + b</returns>
+  double Add(double a, double b);
+
+  /// <summary>Subtract b from a</summary>
+  /// <param name="a">the double a</param>
+  /// <param name="b">the double b</param>
+  /// <returns>a - b</returns>
+  double Subtract(double a, double b);
+}

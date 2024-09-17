fd61fd2cb04d059514685f922c62c6086512f355
hee082024kh <hee082024@gmail.com>
2024-09-17 13:15:07 +0200

    Added user input

--- Program.cs
+++ Program.cs
@@ -6,5 +6,16 @@
         Console.WriteLine(calculator.Add(1, 1));
         Console.WriteLine(calculator.Subtract(70, 1));
 
+        Console.WriteLine("enter value a (number)");
+        int addA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter value b (number)");
+        int addB = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(calculator.Add(addA, addB));
+        
+        Console.WriteLine("enter value a (number)");
+        int subA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter value b (number)");
+        int subB = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(calculator.Subtract(subA, subB));
     }
 }

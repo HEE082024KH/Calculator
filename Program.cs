c0ac5f5ed888464f45152cace573c5de1cb7ddfe
hee082024kh <hee082024@gmail.com>
2024-09-18 12:58:23 +0200

    Extra assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
 {
     static void Main(string[] args)
     {
@@ -7,14 +9,14 @@
         Console.WriteLine(calculator.Add(1, 1));
         Console.WriteLine(calculator.Subtract(70, 1));
 
-        // Brukerinput addere
+        // // Brukerinput addere
         Console.WriteLine("enter value a (number)");
         int addA = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("enter value b (number)");
         int addB = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine(calculator.Add(addA, addB));
         
-        // Brukerinput subtrahere
+        // // Brukerinput subtrahere
         Console.WriteLine("enter value a (number)");
         int subA = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("enter value b (number)");
@@ -22,6 +24,38 @@
         Console.WriteLine(calculator.Subtract(subA, subB));
 
         // Extraoppgave
-        
+        Console.WriteLine("enter value a (number)");
+        int numA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter operator (+, -, * or /)");
+        string? opA = Console.ReadLine();
+        Console.WriteLine("enter value b (number)");
+        int numB = Convert.ToInt32(Console.ReadLine());
+
+        bool Invalid = false;
+
+        while (Invalid == false)
+        switch (opA)
+        {
+            case "+":
+                Console.WriteLine(calculator.Add(numA, numB));
+                Invalid = true;
+                break;
+            case "-":
+                Console.WriteLine(calculator.Subtract(numA, numB));
+                Invalid = true;
+                break;
+            case "*":
+                Console.WriteLine(calculator.Multiply(numA, numB));
+                Invalid = true;
+                break;
+            case "/":
+                Console.WriteLine(calculator.Divide(numA, numB));
+                Invalid = true;
+                break;
+            default:
+                Console.WriteLine("Invalid input. Enter operator (+, -, * or /)");
+                opA = Console.ReadLine();
+                break;
+        }
     }
 }
